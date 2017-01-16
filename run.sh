f879a17640433594770775b5096f91dae1691413
Michael Ohayon <mohayon75@gmail.com>
2017-01-16 19:28:41 +0100

    Added target armv6l

--- run.sh
+++ run.sh
@@ -1,13 +1,35 @@
+USER="mikkl"
+
 for template in templates/*
 do
-    ARCHS=("amd64" "arm64" "armhf" "i386")
-    
-    for arch in "${ARCHS[@]}"
-    do  
-	sed "s/{ARCH}/$arch/g" "$template" > .dockerfile.tmp
-	imageName=`basename $template`:"$arch"
-	docker rmi "mikkl/$imageName"
-	docker build -t mikkl/"$imageName" -f .dockerfile.tmp . 
-	docker push mikkl/"$imageName"
+    ARCHS=("amd64" "arm64" "armhf" "i386" "armv6l")
+    IMAGES=("multiarch\/ubuntu-core:amd64-xenial" "multiarch\/ubuntu-core:arm64-xenial" "multiarch\/ubuntu-core:armhf-xenial" "multiarch\/ubuntu-core:i386-xenial" "resin\/rpi-raspbian:jessie")
+    for i in "${!ARCHS[@]}"
+    do
+      arch=${ARCHS[$i]}
+      image=${IMAGES[$i]}
+
+      outputImageName="$USER"/`basename $template`:"$arch"
+      sed "s/{IMAGE}/$image/g" "$template" > .dockerfile.tmp
+
+      echo "-- Creating $outputImageName --"
+      read -n 1 -p "Remove old docker image ? y/n " yn
+      echo ""
+      case $yn in
+       [Yy]* ) docker rmi $outputImageName;;
+      esac
+
+      read -n 1 -p "Build docker image ? y/n " yn
+      echo ""
+      case $yn in
+       [Yy]* ) docker build -t $outputImageName -f .dockerfile.tmp .;;
+      esac
+
+      read -n 1 -p "Push docker image ? y/n " yn
+      echo ""
+      case $yn in
+       [Yy]* ) docker push $outputImageName;;
+      esac
+
     done
 done
